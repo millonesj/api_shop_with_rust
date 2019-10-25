e515ca3df79c52ba129d0a369c7c493236cdbe28
Jhonny Neciosup Millones <jhonny.neciosup@indagia.com>
2019-10-25 18:32:40 -0500

    refactor(diesel): add SQL for our products schema

--- migrations/2019-10-25-095754_db-shop/up.sql
+++ migrations/2019-10-25-095754_db-shop/up.sql
@@ -1,1 +1,8 @@
 -- Your SQL goes here
+CREATE TABLE products (
+  id INT(11) PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(100) NOT NULL,
+  price DECIMAL(5,2) NOT NULL,
+  currency CHAR(3) NOT NULL,
+  owner INT(11) NOT NULL
+)
